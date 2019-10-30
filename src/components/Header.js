8fba59262258758179dcd1d66b8761784479de5d
Sam Jespersen <heyheysamj@gmail.com>
2019-10-30 15:39:19 -0700

    modularization

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return <header>
+      <h1>Best Characters</h1>
+    </header>;
+  }
+}
